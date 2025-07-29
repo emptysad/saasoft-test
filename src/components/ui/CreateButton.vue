a8dd25647d9190ddfe2fcd98c8366af2b27eb1e0
Gleb Cherepanov <emptysad@proton.me>
2025-07-30 04:06:58 +0500

    creating create button

---
+++ src/components/ui/CreateButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <Button
+    outlined
+    class="text-black! border-surface-300! aspect-square! rounded-sm!"
+    @click="isCreatingRecord = !isCreatingRecord"
+  >
+    <i
+      class="pi pi-plus text-sm! transition-[rotate] duration-150"
+      :class="{ 'rotate-45': isCreatingRecord }"
+    ></i>
+  </Button>
+</template>
+
+<script setup lang="ts">
+import { Button } from "primevue";
+
+const isCreatingRecord = defineModel<boolean>();
+</script>
+
+<style scoped></style>
