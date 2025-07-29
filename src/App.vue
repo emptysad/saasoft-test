0b9b9df9649053e6efb0bd1bb3d59763151f49ed
Gleb Cherepanov <emptysad@proton.me>
2025-07-30 04:07:29 +0500

    adding create button and rows on page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div></div>
   <div class="container mx-auto flex flex-col gap-5 mt-5">
     <header class="flex items-center gap-5">
       <h1 class="font-bold text-xl">Учётные записи</h1>
+      <CreateButton v-model="isCreatingRecord" />
     </header>
 
     <main>
@@ -16,6 +16,16 @@
       </Message>
 
       <TableComp class="mt-5">
+        <ExistingRecordComp
+          v-for="(record, i) in recordsStore.records"
+          :key="record.uuid"
+          :index="i"
+        >
+        </ExistingRecordComp>
+        <CreateRecordComp
+          v-if="isCreatingRecord"
+          @cancel-creating="isCreatingRecord = false"
+        ></CreateRecordComp>
       </TableComp>
     </main>
   </div>
@@ -24,8 +34,14 @@
 <script setup lang="ts">
 import { Message } from "primevue";
 import TableComp from "./components/table/TableComp.vue";
+import ExistingRecordComp from "./components/table/cells/ExistingRecordComp.vue";
+import CreateRecordComp from "./components/table/cells/CreateRecordComp.vue";
+import CreateButton from "./components/ui/CreateButton.vue";
+import { useRecordsStore } from "./models/useRecordsStore";
 import { ref } from "vue";
 
+const recordsStore = useRecordsStore();
+const isCreatingRecord = ref(false);
 </script>
 
 <style scoped></style>
