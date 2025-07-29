62a9fc3b5439034a170731311bfad50f6d56f5ca
Gleb Cherepanov <emptysad@proton.me>
2025-07-30 03:56:18 +0500

    creating table component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
   <div></div>
+  <div class="container mx-auto flex flex-col gap-5 mt-5">
+    <header class="flex items-center gap-5">
+      <h1 class="font-bold text-xl">Учётные записи</h1>
+    </header>
+
+    <main>
+      <Message severity="secondary">
+        <template #icon>
+          <i class="pi pi-question-circle text-2xl!"></i>
+        </template>
+        <span>
+          Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;
+        </span>
+      </Message>
+
+      <TableComp class="mt-5">
+      </TableComp>
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { Message } from "primevue";
+import TableComp from "./components/table/TableComp.vue";
+import { ref } from "vue";
+
 </script>
 
 <style scoped></style>
---
+++ src/components/table/TableComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid gap-3 grid-cols-[1fr_1fr_1fr_1fr_auto]">
+    <div class="text-sm text-surface-400">Метки</div>
+    <div class="text-sm text-surface-400">Тип записи</div>
+    <div class="text-sm text-surface-400">Логин</div>
+    <div class="text-sm text-surface-400">Пароль</div>
+    <div class="text-sm text-surface-400"></div>
+
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
